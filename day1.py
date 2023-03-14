abea612db5680d7c01e9b2504a374780e5a1a1d4
dahiya2772004 <77168048+dahiya2772004@users.noreply.github.com>
2023-03-14 08:43:21 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,7 @@
+#this is day 1 project of 100 days of code by Angela Yu
+#this is band name generator 
+print("welcome to band name generator") #title
+cityYouLiveIn= input("which city do you live in?")     #input for city name
+nameOfYourPet=input("what is your pet name?")   #asking the user where he grew up 
+print("your band name could be "+ cityYouLiveIn +" "+nameOfYourPet) #combining everything
+
